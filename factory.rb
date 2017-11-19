e761012876c55340e243d775f34ebef3714d8a8c
Dmitriy <30774291+Owatanka@users.noreply.github.com>
2017-11-19 20:01:25 +0200

    Update factory.rb

--- factory.rb
+++ factory.rb
@@ -1,56 +1,90 @@
 class Factory
   def self.new(*args, &block)
-  
-    Class.new do
-      attr_accessor(*args)
+      class_name = if args.first.is_a? String
+                   unless args.first.match(/^[A-Z]/)
+                     raise(NameError, "identifier #{args.first} needs to be constant")
+                   end
+                   args.shift
+                   end
+      new_class = Factory.const_set(class_name, Class.new{
+      attr_accessor *args
       
-      def initialize(*vars)
-        args.each_index { |index| instance_variable_set(:"@#{args[index]}", vars[index]) }
-      end
+      define_method :initialize do |*vars|
+        raise ArgumentError if vars.size > args.size
 
-      define_method :args do
-        args
+        args.each_with_index do |var, index|
+          instance_variable_set "@#{var}", vars[index]
       end
-      
+      end
+      })
+
       def ==(obj)
         self.class == obj.class && values == obj.values
       end
 
       def [](arg)
-        #if arg.is_a?(Integer)
-        instance_variable_get("@#{arg}")
-        end
+        arg.is_a?(Integer) ? to_a[arg] : send(arg.to_sym)
       end
 
       def []=(arg, value)
         instance_variable_set("@#{arg}", value)
+      end
+      
+      def dig(*args)
+          hash.dig(*args)
       end
   
 
       def each(&block)
         values.each(&block)
       end    
-
-      def length
-        args.size
+      
+      def each_pair(&name_block)
+        to_h.each_pair(&name_block)
       end
 
+      def length
+        self.values.length
+      end
+      
+      def members
+        instance_variables.map { |a| ("#{a}"[1..-1]).to_sym }
+      end
+      
       def select(&block)
         values.select(&block)
+      end
+      
+      def dig(*keys)
+        to_h.dig(*keys)
+      end
+      
+      def each_pair(&block)
+        to_h.each_pair(&block)
       end
 
       def values
         instance_variables.map { |arg| instance_variable_get("#{arg}") }
       end
 
-      def values_at(*selector)
-        values.values_at(*selector)
+      def values_at(*select)
+        values.values_at(*select)
       end
+    
+      def hash
+        Hash[instance_variables.map { |name| [name.to_s.delete('@').to_sym, instance_variable_get(name)]}]
+      end
+      
+      
+
 
       alias_method :to_a, :values 
       alias_method :to_s, :inspect
       alias_method :size, :length
-
-    end
-  end
-  end
+      alias_method :to_h, :hash
+      
+      
+      class_eval(&block) if block_given?
+end
+    
+end
